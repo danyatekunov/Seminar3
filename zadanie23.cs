b3ad4ece1fc229b9701e824ceadc672aa587d7fa
danyatekunov <121130644+danyatekunov@users.noreply.github.com>
2023-01-22 12:36:37 +0300

    Add files via upload

---
+++ zadanie23.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void CubeCalc()
+{
+
+    for (int i = 1; i <= x; i++)
+    {
+
+        Console.WriteLine($"{i * i * i} ");
+    }
+}
+CubeCalc();
